b02e6b5a52be069b9dcf409647c4fd2bbefe0feb
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-18 02:28:38 -0400

    java2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # De-Master-Completo-en-Java-de-cero-a-experto-2024-127-hrs-Part-2
+
---
+++ Sección 41 - Introducción a Java EE - ahora renombrado a Jakarta EE/2. Primer ejemplo con Servlet/java/org.aguzman.webapp.servlet/HolaMundoServlet.java
@@ -0,0 +1,11 @@
+
+
+
+package org.aguzman.webapp.servlet;
+
+import jakarta.servlet.annotation.WebServlet; // Importa la anotación @WebServlet
+
+@WebServlet("/hola-mundo") // Mapea este Servlet a la ruta /hola-mundo
+public class HolaMundoServlet {
+    // ... cuerpo de la clase ...
+}
