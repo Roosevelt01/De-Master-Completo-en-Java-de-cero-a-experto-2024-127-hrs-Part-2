166e5eee7a8f2cd953a44447fbf5cc2c718cb1c6
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-18 02:39:51 -0400

    java2

--- Sección 41 - Introducción a Java EE - ahora renombrado a Jakarta EE/2. Primer ejemplo con Servlet/java/org.aguzman.webapp.servlet/HolaMundoServlet.java
+++ Sección 41 - Introducción a Java EE - ahora renombrado a Jakarta EE/2. Primer ejemplo con Servlet/java/org.aguzman.webapp.servlet/HolaMundoServlet.java
@@ -1,8 +1,12 @@
 package org.aguzman.webapp.servlet;
 
 import jakarta.servlet.annotation.WebServlet; // Importa la anotación @WebServlet
+import jakarta.servlet.http.HttpServlet; // Importa la clase base para Servlets HTTP
 
 @WebServlet("/hola-mundo") // Mapea este Servlet a la ruta /hola-mundo
-public class HolaMundoServlet {
+public class HolaMundoServlet extends HttpServlet{
     // ... cuerpo de la clase ...
 }
+
+
+
