84c631ffd4e6db1170b478a186eb74c508ee84d1
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-20 02:05:23 -0400

    java2

--- 2. Sección 42: Jakarta EE 9: API Servlet y Manejo de formularios/2. Creando la clase servlet para procesar el formulario/src/main/java/org/aguzman/apiservlet/webapp/form/FormServlet.java
+++ 2. Sección 42: Jakarta EE 9: API Servlet y Manejo de formularios/2. Creando la clase servlet para procesar el formulario/src/main/java/org/aguzman/apiservlet/webapp/form/FormServlet.java
@@ -7,9 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.PrintWriter; // Necesario para escribir la respuesta
 
-@WebServlet("/registro")
+@WebServlet("/registro")// Anotación para mapear el Servlet a una URL
 public class FormServlet extends HttpServlet {
 
     @Override
