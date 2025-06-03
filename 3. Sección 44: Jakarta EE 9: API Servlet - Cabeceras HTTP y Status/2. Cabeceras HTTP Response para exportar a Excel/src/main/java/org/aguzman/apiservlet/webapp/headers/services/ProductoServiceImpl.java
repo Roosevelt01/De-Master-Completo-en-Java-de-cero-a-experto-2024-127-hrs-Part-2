ce9eec195e59a0f861386c99a45bc914600593c6
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-06-02 21:27:39 -0400

    flutter

---
+++ 3. Sección 44: Jakarta EE 9: API Servlet - Cabeceras HTTP y Status/2. Cabeceras HTTP Response para exportar a Excel/src/main/java/org/aguzman/apiservlet/webapp/headers/services/ProductoServiceImpl.java
@@ -0,0 +1,33 @@
+package org.aguzman.apiservlet.webapp.headers.services;
+
+import org.aguzman.apiservlet.webapp.headers.models.Producto;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductoServiceImpl implements ProductoService {
+
+    @Override
+    public List<Producto> listar() {
+        return Arrays.asList(new Producto(1L, "notebook", "computación", 175000),
+                new Producto(2L, "mesa escritorio", "oficina", 100000),
+                new Producto(3L, "tecldo mecánico", "computación", 40000));
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
