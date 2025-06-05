578b451661a4c450d3b9e840719112d68e23028c
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-06-05 18:23:46 -0400

    java2

--- 3. Sección 44: Jakarta EE 9: API Servlet - Cabeceras HTTP y Status/5. Cabeceras HTTP Response Location vs Dispatcher forward/main/java/org/aguzman/apiservlet/webapp/headers/controllers/RedirigirServlet.java
+++ 3. Sección 44: Jakarta EE 9: API Servlet - Cabeceras HTTP y Status/5. Cabeceras HTTP Response Location vs Dispatcher forward/main/java/org/aguzman/apiservlet/webapp/headers/controllers/RedirigirServlet.java
@@ -8,14 +8,17 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-//Paso 3
 @WebServlet("/redirigir")
 public class RedirigirServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+    throws ServletException, IOException {
+            // Opción 1 (Manual): Establecer la cabecera Location y el estado HTTP
             //resp.setHeader("Location", req.getContextPath()+ "/productos.html");
             //resp.setStatus(HttpServletResponse.SC_FOUND);
+
+             // Opción 2 (Recomendada): Usar el método sendRedirect()
             resp.sendRedirect(req.getContextPath()+ "/productos.html");
     }
 }
