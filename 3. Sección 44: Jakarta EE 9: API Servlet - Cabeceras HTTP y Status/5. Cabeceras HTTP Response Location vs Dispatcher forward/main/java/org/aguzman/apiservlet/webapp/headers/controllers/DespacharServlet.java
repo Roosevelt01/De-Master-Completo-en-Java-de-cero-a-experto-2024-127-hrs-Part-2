2eac862615e15da4d61308bb6a21908be1cc56ae
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-06-05 18:41:25 -0400

    java2

--- 3. Sección 44: Jakarta EE 9: API Servlet - Cabeceras HTTP y Status/5. Cabeceras HTTP Response Location vs Dispatcher forward/main/java/org/aguzman/apiservlet/webapp/headers/controllers/DespacharServlet.java
+++ 3. Sección 44: Jakarta EE 9: API Servlet - Cabeceras HTTP y Status/5. Cabeceras HTTP Response Location vs Dispatcher forward/main/java/org/aguzman/apiservlet/webapp/headers/controllers/DespacharServlet.java
@@ -6,14 +6,18 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//Paso 6
 import java.io.IOException;
 
 @WebServlet("/despachar")
 public class DespacharServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+        throws ServletException, IOException {
+        // Obtener el RequestDispatcher para el recurso de destino.
+        // La ruta es relativa al contexto de la aplicación, sin el nombre del contexto.
+        // Es la ruta interna del Servlet o JSP (ej., "/productos.html").
+        // Luego, se "reenvía" (forward) la petición y respuesta actuales al nuevo recurso.
         getServletContext().getRequestDispatcher("/productos.html").forward(req, resp);
     }
 }
