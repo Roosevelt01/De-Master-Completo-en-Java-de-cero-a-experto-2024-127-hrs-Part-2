140fecace5656aab95dddffafe32b99c929b55f7
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-06-11 19:48:52 -0400

    java2

--- 4. Sección 45: Jakarta EE9 - API Servlet menaje de Cookies/3. Sistema de login con cookies parte 2 protegiendo recursos/src/main/java/org/aguzman/apiservlet/webapp/headers/services/LoginServiceImpl.java
+++ 4. Sección 45: Jakarta EE9 - API Servlet menaje de Cookies/3. Sistema de login con cookies parte 2 protegiendo recursos/src/main/java/org/aguzman/apiservlet/webapp/headers/services/LoginServiceImpl.java
@@ -6,8 +6,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.Optional;
 
-//Paso 2
+// Esta clase implementa la interfaz `LoginService`, lo que significa que debe proporcionar una implementación
+// concreta para todos los métodos declarados en esa interfaz.
 public class LoginServiceImpl implements LoginService {
+    
+    // declarado en la interfaz `LoginService`. Es una buena práctica para asegurar que la firma del
+    // método coincida con la de la interfaz.
     @Override
     public Optional<String> getUsername(HttpServletRequest req) {
         Cookie[] cookies = req.getCookies() != null? req.getCookies():new Cookie[0];
