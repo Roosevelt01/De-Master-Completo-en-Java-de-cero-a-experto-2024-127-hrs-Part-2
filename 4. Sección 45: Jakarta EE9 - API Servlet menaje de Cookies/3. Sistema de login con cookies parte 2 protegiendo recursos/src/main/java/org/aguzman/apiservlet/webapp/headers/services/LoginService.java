ec46a1b72d33806e77b73afedec6ad22fea99c83
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-06-11 19:35:22 -0400

    java2

--- 4. Sección 45: Jakarta EE9 - API Servlet menaje de Cookies/3. Sistema de login con cookies parte 2 protegiendo recursos/src/main/java/org/aguzman/apiservlet/webapp/headers/services/LoginService.java
+++ 4. Sección 45: Jakarta EE9 - API Servlet menaje de Cookies/3. Sistema de login con cookies parte 2 protegiendo recursos/src/main/java/org/aguzman/apiservlet/webapp/headers/services/LoginService.java
@@ -4,7 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Optional;
 
-//Paso 1
+// Esta interfaz define el contrato que deben seguir las implementaciones del servicio de login.
 public interface LoginService {
+
+    // Declara el método getUsername que debe ser implementado
     Optional<String> getUsername(HttpServletRequest request);
 }
+
