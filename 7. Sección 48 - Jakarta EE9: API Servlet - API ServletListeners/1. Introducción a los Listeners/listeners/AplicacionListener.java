91fa834d36b049a7d74f3684231e54acaeea4a1d
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-06-23 19:39:23 -0400

    java2

---
+++ 7. Sección 48 - Jakarta EE9: API Servlet - API ServletListeners/1. Introducción a los Listeners/listeners/AplicacionListener.java
@@ -0,0 +1,49 @@
+package org.aguzman.apiservlet.webapp.headers.listeners;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebListener;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+
+@WebListener
+public class AplicacionListener implements ServletContextListener,
+        ServletRequestListener, HttpSessionListener {
+
+    private ServletContext servletContext;
+
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        sce.getServletContext().log("inicializando la aplicación!");
+        servletContext = sce.getServletContext();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        servletContext.log("destruyendo la aplicación!");
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+        servletContext.log("inicializando la aplicación!");
+    }
+
+
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        servletContext.log("destruyendo la aplicación!");
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        servletContext.log("inicializando la aplicación!");
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        servletContext.log("destruyendo la aplicación!");
+    }
+
+
+}
