9cac48f3c97c259595bd0597ed59008523f0aaef
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-06-23 14:37:11 -0400

    java2

--- 6. Sección 47 - Jakarta EE9: API Servlet - Carro de compras con HttpSession/3. Ver carro de compras/src/main/java/org/aguzman/apiservlet/webapp/headers/controllers/VerCarroServlet.java
+++ 6. Sección 47 - Jakarta EE9: API Servlet - Carro de compras con HttpSession/3. Ver carro de compras/src/main/java/org/aguzman/apiservlet/webapp/headers/controllers/VerCarroServlet.java
@@ -8,11 +8,16 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+// 1. Anotación para mapear la URL
 @WebServlet("/ver-carro")
+// 2. Extender de HttpServlet
 public class VerCarroServlet extends HttpServlet {
 
+    // 3. Sobrescritura del método doGet
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+    throws ServletException, IOException {
+        // 4. Despachar la solicitud a la vista JSP
         getServletContext().getRequestDispatcher("/carro.jsp").forward(req, resp);
     }
 }
