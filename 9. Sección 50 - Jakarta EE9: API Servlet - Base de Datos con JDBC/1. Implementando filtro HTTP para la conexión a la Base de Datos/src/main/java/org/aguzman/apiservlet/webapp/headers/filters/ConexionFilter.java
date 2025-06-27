2b953e981588b22cb5d924f33d3b12e18bdc285c
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-06-27 04:36:14 -0400

    java2

--- 9. Sección 50 - Jakarta EE9: API Servlet - Base de Datos con JDBC/1. Implementando filtro HTTP para la conexión a la Base de Datos/src/main/java/org/aguzman/apiservlet/webapp/headers/filters/ConexionFilter.java
+++ 9. Sección 50 - Jakarta EE9: API Servlet - Base de Datos con JDBC/1. Implementando filtro HTTP para la conexión a la Base de Datos/src/main/java/org/aguzman/apiservlet/webapp/headers/filters/ConexionFilter.java
@@ -12,7 +12,8 @@ import java.sql.SQLException;
 @WebFilter("/*")
 public class ConexionFilter implements Filter {
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+     throws IOException, ServletException {
 
         try(Connection conn = ConexionBaseDatos.getConnection()){
             if(conn.getAutoCommit()){
--- 9. Sección 50 - Jakarta EE9: API Servlet - Base de Datos con JDBC/1. Implementando filtro HTTP para la conexión a la Base de Datos/src/main/java/org/aguzman/apiservlet/webapp/headers/util/ConexionBaseDatos.java
+++ 9. Sección 50 - Jakarta EE9: API Servlet - Base de Datos con JDBC/1. Implementando filtro HTTP para la conexión a la Base de Datos/src/main/java/org/aguzman/apiservlet/webapp/headers/util/ConexionBaseDatos.java
@@ -1,14 +1,23 @@
+
+
 package org.aguzman.apiservlet.webapp.headers.util;
 
 import java.sql.*;
 
+//Clase de utilidad para gestionar la creación de conexiones a la base de datos.
+//Centraliza los parámetros de conexión (URL, usuario, contraseña).
+
 public class ConexionBaseDatos {
+    // Parámetros de la conexión como atributos estáticos privados.
     private static String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=America/Santiago";
     private static String username = "root";
     private static String password = "root";
 
     public static Connection getConnection() throws SQLException {
+        // Se utiliza el DriverManager para obtener la conexión usando los parámetros definidos.
         return DriverManager.getConnection(url, username, password);
     }
 
 }
+
+
