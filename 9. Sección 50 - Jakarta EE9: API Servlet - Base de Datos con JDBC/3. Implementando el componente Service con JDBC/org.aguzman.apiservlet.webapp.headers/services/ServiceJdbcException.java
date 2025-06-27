2d2c17e85862229db543e9e5c07f44c2eaa882e6
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-06-27 18:29:01 -0400

    java2

--- 9. Sección 50 - Jakarta EE9: API Servlet - Base de Datos con JDBC/3. Implementando el componente Service con JDBC/org.aguzman.apiservlet.webapp.headers/services/ServiceJdbcException.java
+++ 9. Sección 50 - Jakarta EE9: API Servlet - Base de Datos con JDBC/3. Implementando el componente Service con JDBC/org.aguzman.apiservlet.webapp.headers/services/ServiceJdbcException.java
@@ -1,10 +1,12 @@
 package org.aguzman.apiservlet.webapp.headers.services;
 
 public class ServiceJdbcException extends  RuntimeException{
+    // Al heredar de RuntimeException, se convierte en una excepción "unchecked".
     public ServiceJdbcException(String message) {
         super(message);
     }
 
+    // Este constructor es clave: permite "envolver" la excepción original (la causa).
     public ServiceJdbcException(String message, Throwable cause) {
         super(message, cause);
     }
