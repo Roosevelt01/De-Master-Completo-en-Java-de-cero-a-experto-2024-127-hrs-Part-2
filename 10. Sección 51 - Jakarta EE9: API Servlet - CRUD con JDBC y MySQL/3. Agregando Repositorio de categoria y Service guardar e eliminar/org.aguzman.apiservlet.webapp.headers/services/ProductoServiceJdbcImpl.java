11d9345ccae3cd8de934859cfb871f9da6650408
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-07-09 06:55:28 -0400

    java2

---
+++ 10. Sección 51 - Jakarta EE9: API Servlet - CRUD con JDBC y MySQL/3. Agregando Repositorio de categoria y Service guardar e eliminar/org.aguzman.apiservlet.webapp.headers/services/ProductoServiceJdbcImpl.java
@@ -0,0 +1,80 @@
+package org.aguzman.apiservlet.webapp.headers.services;
+
+import org.aguzman.apiservlet.webapp.headers.models.Categoria;
+import org.aguzman.apiservlet.webapp.headers.models.Producto;
+import org.aguzman.apiservlet.webapp.headers.repositories.ProductoRepositoryJdbcImpl;
+import org.aguzman.apiservlet.webapp.headers.repositories.Repository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoServiceJdbcImpl implements ProductoService{
+    private Repository<Producto> repositoryJdbc;
+    private Repository<Categoria> repositoryCategoriaJdbc;
+
+    public ProductoServiceJdbcImpl(Connection connection) {
+        this.repositoryJdbc = new ProductoRepositoryJdbcImpl(connection);
+    }
+
+    @Override
+    public List<Producto> listar() {
+        try {
+            return repositoryJdbc.listar();
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+    @Override
+    public Optional<Producto> porId(Long id) {
+        try {
+            return Optional.ofNullable(repositoryJdbc.porId(id));
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+    //Paso 1
+    @Override
+    public void eliminar(Long id) {
+        try {
+            repositoryJdbc.eliminar(id);
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+    //Paso 2
+    @Override
+    public void guardar(Producto producto) {
+        try {
+            repositoryJdbc.guardar(producto);
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+    //Paso 3
+    @Override
+    public List<Categoria> listarCategoria() {
+        try {
+            return repositoryCategoriaJdbc.listar();
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+    //Paso 4
+    @Override
+    public Optional<Categoria> porIdCategoria(Long id) {
+        try {
+            return Optional.ofNullable(repositoryCategoriaJdbc.porId(id));
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+
+}
