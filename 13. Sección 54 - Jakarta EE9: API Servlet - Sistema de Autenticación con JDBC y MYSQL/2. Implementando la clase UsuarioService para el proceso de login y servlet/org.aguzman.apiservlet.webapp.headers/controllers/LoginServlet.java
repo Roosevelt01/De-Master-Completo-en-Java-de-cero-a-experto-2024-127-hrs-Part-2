63bfbc242ddb80de73a43e3a41bd8253cd023f8b
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-07-30 02:10:31 -0400

    java2

---
+++ 13. Sección 54 - Jakarta EE9: API Servlet - Sistema de Autenticación con JDBC y MYSQL/2. Implementando la clase UsuarioService para el proceso de login y servlet/org.aguzman.apiservlet.webapp.headers/controllers/LoginServlet.java
@@ -0,0 +1,69 @@
+package org.aguzman.apiservlet.webapp.headers.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import org.aguzman.apiservlet.webapp.headers.models.Usuario;
+import org.aguzman.apiservlet.webapp.headers.services.LoginService;
+import org.aguzman.apiservlet.webapp.headers.services.LoginServiceSessionImpl;
+import org.aguzman.apiservlet.webapp.headers.services.UsuarioService;
+import org.aguzman.apiservlet.webapp.headers.services.UsuarioServiceImpl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.Optional;
+
+@WebServlet({"/login", "/login.html"})
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LoginService auth = new LoginServiceSessionImpl();
+        Optional<String> usernameOptional = auth.getUsername(req);
+
+        if(usernameOptional.isPresent()){
+            resp.setContentType("text/html;charset=UTF-8");
+            try (PrintWriter out = resp.getWriter()) {
+
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("     <head>");
+                out.println("         <meta charset=\"UTF-8\">");
+                out.println("         <title>Hola " + usernameOptional.get() + "</title>");
+                out.println("     </head>");
+                out.println("     <body>");
+                out.println("         <h1>Hola " + usernameOptional.get() + " y has inciado sesión con éxito! </h1>");
+                out.println("<p><a href='"+req.getContextPath() +"/index.jsp'>Volver</a></p>");
+                out.println("<p><a href='"+req.getContextPath() +"/logout'>Cerrar sesión</a></p>");
+                out.println("     </body>");
+                out.println("</html>");
+            }
+        }else{
+            req.setAttribute("title", req.getAttribute("title") + ": Login");//Paso 1
+            getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        //Paso 1
+        UsuarioService service = new UsuarioServiceImpl((Connection) req.getAttribute("conn"));
+
+        //Paso 2
+        Optional<Usuario> usuarioOptional = service.login(username, password);
+
+        if (usuarioOptional.isPresent()) {//Paso 3
+
+            HttpSession session = req.getSession();
+            session.setAttribute("username", username);
+
+            resp.sendRedirect(req.getContextPath() + "/login.html");
+        } else {
+            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Loco sentimos no esta autorizado para ingresar a esta página!");
+        }
+    }
+}
