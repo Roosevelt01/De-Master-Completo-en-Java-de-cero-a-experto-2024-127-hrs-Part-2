11ada376a9ec0c34d54e5ec140a1e457c2c6e27d
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-07-30 02:42:49 -0400

    java2

--- 13. Sección 54 - Jakarta EE9: API Servlet - Sistema de Autenticación con JDBC y MYSQL/2. Implementando la clase UsuarioService para el proceso de login y servlet/org.aguzman.apiservlet.webapp.headers/services/UsuarioService.java
+++ 13. Sección 54 - Jakarta EE9: API Servlet - Sistema de Autenticación con JDBC y MYSQL/2. Implementando la clase UsuarioService para el proceso de login y servlet/org.aguzman.apiservlet.webapp.headers/services/UsuarioService.java
@@ -5,5 +5,12 @@ import org.aguzman.apiservlet.webapp.headers.models.Usuario;
 import java.util.Optional;
 
 public interface UsuarioService {
+        /**
+     * Intenta autenticar a un usuario con el nombre de usuario y contraseña proporcionados.
+     * username El nombre de usuario.
+     * password La contraseña.
+     * @return Un Optional que contiene el objeto Usuario si la autenticación es exitosa,
+     * o un Optional vacío si las credenciales no coinciden o el usuario no existe.
+     */
     Optional<Usuario> login(String username, String password);
 }
