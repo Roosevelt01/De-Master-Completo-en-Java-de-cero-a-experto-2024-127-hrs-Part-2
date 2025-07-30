f5b9eff43acd36e77fda6e3bc9e466b686331509
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-07-30 03:59:16 -0400

    java2

---
+++ 13. Sección 54 - Jakarta EE9: API Servlet - Sistema de Autenticación con JDBC y MYSQL/3. Complete Code/main/java/org/aguzman/apiservlet/webapp/headers/models/Producto.java
@@ -0,0 +1,73 @@
+package org.aguzman.apiservlet.webapp.headers.models;
+
+import java.time.LocalDate;
+
+public class Producto {
+    private Long id;
+    private String nombre;
+    private Categoria categoria;//Paso 1
+    private int precio;
+    private String sku;//Paso 2
+    private LocalDate fechaRegistro;//Paso 3
+
+    public Producto() {
+    }
+
+    public Producto(Long id, String nombre, String tipo, int precio) {
+        this.id = id;
+        this.nombre = nombre;
+        Categoria categoria = new Categoria();//Paso 4
+        categoria.setNombre(tipo);//Paso 5
+        this.categoria = categoria;//Paso 6
+        this.precio = precio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    //Paso 7
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+}
