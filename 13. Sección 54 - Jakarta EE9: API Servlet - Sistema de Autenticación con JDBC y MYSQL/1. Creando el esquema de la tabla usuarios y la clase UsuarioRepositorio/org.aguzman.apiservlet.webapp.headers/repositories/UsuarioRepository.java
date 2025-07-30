281faf701cd47d9a7af89079413e8d4e36260397
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-07-29 20:48:20 -0400

    java2

--- 13. Sección 54 - Jakarta EE9: API Servlet - Sistema de Autenticación con JDBC y MYSQL/1. Creando el esquema de la tabla usuarios y la clase UsuarioRepositorio/org.aguzman.apiservlet.webapp.headers/repositories/UsuarioRepository.java
+++ 13. Sección 54 - Jakarta EE9: API Servlet - Sistema de Autenticación con JDBC y MYSQL/1. Creando el esquema de la tabla usuarios y la clase UsuarioRepositorio/org.aguzman.apiservlet.webapp.headers/repositories/UsuarioRepository.java
@@ -2,8 +2,11 @@ package org.aguzman.apiservlet.webapp.headers.repositories;
 
 import org.aguzman.apiservlet.webapp.headers.models.Usuario;
 
+// Importa la clase SQLException
 import java.sql.SQLException;
 
+// Extiende la interfaz genérica Repository
 public interface UsuarioRepository extends Repository<Usuario>{
+    // Método para buscar un usuario por su nombre de usuario
     Usuario porUsername(String username) throws SQLException;
 }
