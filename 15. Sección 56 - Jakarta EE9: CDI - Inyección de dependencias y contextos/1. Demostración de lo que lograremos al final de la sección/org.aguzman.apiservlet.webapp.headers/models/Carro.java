8756256680157c22d2792b48d27a839c9357d6dc
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-02 01:29:51 -0400

    java2

--- 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/1. Demostración de lo que lograremos al final de la sección/org.aguzman.apiservlet.webapp.headers/models/Carro.java
+++ 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/1. Demostración de lo que lograremos al final de la sección/org.aguzman.apiservlet.webapp.headers/models/Carro.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-
 // 1. Le da un nombre al bean para usarlo en vistas (JSP)
 @SessionScoped
 // 2. Define su ciclo de vida: una instancia por sesión de usuario
