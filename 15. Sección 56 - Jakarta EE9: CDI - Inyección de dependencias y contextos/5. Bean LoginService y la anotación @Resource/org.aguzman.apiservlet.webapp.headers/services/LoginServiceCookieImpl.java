bfb7d4e4694715541e87bea2945304a8983a218c
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-03 18:57:42 -0400

    java2

---
+++ 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/5. Bean LoginService y la anotación @Resource/org.aguzman.apiservlet.webapp.headers/services/LoginServiceCookieImpl.java
@@ -0,0 +1,22 @@
+package org.aguzman.apiservlet.webapp.headers.services;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Alternative;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+//Paso 1
+@Alternative
+public class LoginServiceCookieImpl implements LoginService {
+    @Override
+    public Optional<String> getUsername(HttpServletRequest req) {
+        Cookie[] cookies = req.getCookies() != null ? req.getCookies(): new Cookie[0];
+        return Arrays.stream(cookies)
+                .filter(c-> "username".equals(c.getName()))
+                .map(Cookie::getValue)
+                .findAny();
+    }
+}
