7c264ddde44270f7b12849b36c9b5d057561b835
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-03 16:16:54 -0400

    java2

--- 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/4. Inyectando beans Services en los Servlets/org.aguzman.apiservlet.webapp.headers/services/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package org.aguzman.apiservlet.webapp.headers.services;
-
-import org.aguzman.apiservlet.webapp.headers.models.Usuario;
-
-import java.util.Optional;
-
-public interface UsuarioService {
-    Optional<Usuario> login(String username, String password);
-}
