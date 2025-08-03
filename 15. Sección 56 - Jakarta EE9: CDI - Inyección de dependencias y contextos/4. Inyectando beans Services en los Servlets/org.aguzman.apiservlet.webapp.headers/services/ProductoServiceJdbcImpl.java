343ae00add0d7afd5575bc624b094f376be7654f
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-03 16:09:10 -0400

    java2

---
+++ 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/4. Inyectando beans Services en los Servlets/org.aguzman.apiservlet.webapp.headers/services/ProductoServiceJdbcImpl.java
@@ -0,0 +1,77 @@
+package org.aguzman.apiservlet.webapp.headers.services;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.aguzman.apiservlet.webapp.headers.models.Categoria;
+import org.aguzman.apiservlet.webapp.headers.models.Producto;
+import org.aguzman.apiservlet.webapp.headers.repositories.Repository;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+//PAso 1
+//@Named("defecto")
+public class ProductoServiceJdbcImpl implements ProductoService{
+    @Inject
+    private Repository<Producto> repositoryJdbc;
+
+    @Inject
+    private Repository<Categoria> repositoryCategoriaJdbc;
+
+    @Override
+    public List<Producto> listar() {
+        try {
+            return repositoryJdbc.listar();
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+    @Override
+    public Optional<Producto> porId(Long id) {
+        try {
+            return Optional.ofNullable(repositoryJdbc.porId(id));
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        try {
+            repositoryJdbc.eliminar(id);
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+    @Override
+    public void guardar(Producto producto) {
+        try {
+            repositoryJdbc.guardar(producto);
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+    @Override
+    public List<Categoria> listarCategoria() {
+        try {
+            return repositoryCategoriaJdbc.listar();
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+    @Override
+    public Optional<Categoria> porIdCategoria(Long id) {
+        try {
+            return Optional.ofNullable(repositoryCategoriaJdbc.porId(id));
+        } catch (SQLException throwables) {
+            throw new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+}
