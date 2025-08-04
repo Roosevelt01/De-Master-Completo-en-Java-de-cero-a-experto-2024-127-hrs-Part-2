689c4df52048e11d2eed9615a39f587c69a1689f
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-04 19:20:37 -0400

    java2

--- 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/6. Calificadores con la anotación @Qualifier/org.aguzman.apiservlet.webapp.headers/configs/MysqlConn.java
+++ 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/6. Calificadores con la anotación @Qualifier/org.aguzman.apiservlet.webapp.headers/configs/MysqlConn.java
@@ -7,9 +7,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-//gemini tienes que conceptualizar todos eso(para que sirve y funcionalidad)
 @Qualifier
 @Retention(RetentionPolicy.RUNTIME)
 @Target({METHOD, FIELD, PARAMETER, TYPE, CONSTRUCTOR})
-public @interface MysqlConn {
-}
+public @interface MysqlConn {}
+
+
