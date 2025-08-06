0262f40bde5047b1667afb25cc399b43db8cdead
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-06 19:02:10 -0400

    java2

--- 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/10. Manejo de interceptores CDI/org.aguzman.apiservlet.webapp.headers/interceptors/Logging.java
+++ 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/10. Manejo de interceptores CDI/org.aguzman.apiservlet.webapp.headers/interceptors/Logging.java
@@ -7,7 +7,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@InterceptorBinding//Paso 1
-@Retention(RetentionPolicy.RUNTIME)//Paso 2
+// 1. Define esta anotación como un enlace para un interceptor.
+@InterceptorBinding
+// 2. Meta-anotaciones estándar para que esté disponible en tiempo de ejecución.
+@Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface Logging {}
+
+
