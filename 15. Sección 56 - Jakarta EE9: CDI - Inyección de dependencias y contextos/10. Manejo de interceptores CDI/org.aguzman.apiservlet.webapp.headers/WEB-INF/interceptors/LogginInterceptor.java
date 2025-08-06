d0becac073f5cb0e897bdb280ac3c54ea10f2148
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-06 18:36:05 -0400

    java2

---
+++ 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/10. Manejo de interceptores CDI/org.aguzman.apiservlet.webapp.headers/WEB-INF/interceptors/LogginInterceptor.java
@@ -0,0 +1,35 @@
+package org.aguzman.apiservlet.webapp.headers.interceptors;
+
+import jakarta.inject.Inject;
+import jakarta.interceptor.AroundInvoke;
+import jakarta.interceptor.Interceptor;
+import jakarta.interceptor.InvocationContext;
+
+import java.util.logging.Logger;
+
+@Logging//Paso 1
+@Interceptor //Paso 2
+public class LogginInterceptor {
+    //Paso 3
+    @Inject//Paso 4
+    private Logger log; // Paso 5
+
+    //Paso: 6
+    @AroundInvoke
+    public Object logging(InvocationContext invocation) throws Exception {
+        //Paso 7
+        log.info("***** Entrando antes de invocar el método "+
+                invocation.getMethod().getName() +
+                " de la clase " + invocation.getMethod().getDeclaringClass());
+
+        // Paso 8
+        Object resultado = invocation.proceed();
+
+        //Paso 9
+        return  resultado;
+
+    }
+
+
+
+}
