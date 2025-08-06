57d0fdcb2a28e6f288990b5b3da973b79a7dc023
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-06 19:08:44 -0400

    java2

--- 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/10. Manejo de interceptores CDI/org.aguzman.apiservlet.webapp.headers/interceptors/LogginInterceptor.java
+++ 15. Sección 56 - Jakarta EE9: CDI - Inyección de dependencias y contextos/10. Manejo de interceptores CDI/org.aguzman.apiservlet.webapp.headers/interceptors/LogginInterceptor.java
@@ -7,29 +7,31 @@ import jakarta.interceptor.InvocationContext;
 
 import java.util.logging.Logger;
 
-@Logging//Paso 1
-@Interceptor //Paso 2
-public class LogginInterceptor {
-    //Paso 3
-    @Inject//Paso 4
-    private Logger log; // Paso 5
 
-    //Paso: 6
+@Logging // 1. Marca esta clase como un Interceptor.
+
+@Interceptor // 2. Enlaza este interceptor a la anotación @Logging.
+public class LogginInterceptor {
+    
+    @inject
+    private Logger log; 
+
+    // 3. Define el método que interceptará las llamadas.
     @AroundInvoke
     public Object logging(InvocationContext invocation) throws Exception {
-        //Paso 7
+        // 4. Lógica ANTES de llamar al método original.
         log.info("***** Entrando antes de invocar el método "+
                 invocation.getMethod().getName() +
                 " de la clase " + invocation.getMethod().getDeclaringClass());
 
-        // Paso 8
+        // 5. Invoca el método original y captura su resultado.
         Object resultado = invocation.proceed();
 
-        //Paso 9
+        // 6. Lógica DESPUÉS de llamar al método original.
+        log.info("***** Saliendo de la invocación del método "
+                + invocation.getMethod().getName());
+
+        // 7. Devuelve el resultado del método original.
         return  resultado;
-
     }
-
-
-
 }
