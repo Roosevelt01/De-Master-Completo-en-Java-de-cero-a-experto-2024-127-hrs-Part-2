c2f579a767960b7726ec11fce71452a61903fefc
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-10 16:04:37 -0400

    java2

--- 16. Sección 57 - Jakarta EE9: JPA e Hibernate/3. Sentencia HQL select y configurando el EntityManagerFactory/src/main/java/org/aguzman/hibernateapp/HibernateListar.java
+++ 16. Sección 57 - Jakarta EE9: JPA e Hibernate/3. Sentencia HQL select y configurando el EntityManagerFactory/src/main/java/org/aguzman/hibernateapp/HibernateListar.java
@@ -8,8 +8,13 @@ import java.util.List;
 
 public class HibernateListar {
     public static void main(String[] args) {
+        // 1. Obtener una instancia del EntityManager
         EntityManager em = JpaUtil.getEntityManager();
+        // 2. Crear y ejecutar la consulta HQL
         List<Cliente> clientes = em.createQuery("select c  from Cliente c").getResultList();
+        // 3. Imprimir los resultados utilizando un método de referencia
         clientes.forEach(System.out::println);
+        // 4. Cerrar el EntityManager
+        em.close();
     }
 }
--- 16. Sección 57 - Jakarta EE9: JPA e Hibernate/3. Sentencia HQL select y configurando el EntityManagerFactory/src/main/java/org/aguzman/hibernateapp/util/JpaUtil.java
+++ 16. Sección 57 - Jakarta EE9: JPA e Hibernate/3. Sentencia HQL select y configurando el EntityManagerFactory/src/main/java/org/aguzman/hibernateapp/util/JpaUtil.java
@@ -5,13 +5,18 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JpaUtil {
+    // 1. Atributo estático y final para garantizar una única instancia del Factory.
     private static final EntityManagerFactory entiryManagerFactory = buildEntityManagerFactory();
 
+    // 2. Método privado para construir el EntityManagerFactory.
     private static EntityManagerFactory buildEntityManagerFactory(){
+        // Usa la clase Persistence para leer el persistence.xml y crear el Factory.
         return Persistence.createEntityManagerFactory("ejemploJPA");
     }
 
+    // 3. Método público y estático para obtener un EntityManager.
     public static EntityManager getEntityManager(){
+        // El Factory crea una nueva instancia de EntityManager para cada solicitud.
         return entiryManagerFactory.createEntityManager();
     }
 }
