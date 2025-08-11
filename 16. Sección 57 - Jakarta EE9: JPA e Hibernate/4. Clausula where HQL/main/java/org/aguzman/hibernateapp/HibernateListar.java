3713b867e517ceef945866f4217ff6ecc2e255eb
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-11 12:20:59 -0400

    java2

--- 16. Sección 57 - Jakarta EE9: JPA e Hibernate/4. Clausula where HQL/main/java/org/aguzman/hibernateapp/HibernateListar.java
+++ 16. Sección 57 - Jakarta EE9: JPA e Hibernate/4. Clausula where HQL/main/java/org/aguzman/hibernateapp/HibernateListar.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class HibernateListar {
     public static void main(String[] args) {
         EntityManager em = JpaUtil.getEntityManager();
+        // Se añade Cliente.class para especificar el tipo de resultado.
         List<Cliente> clientes = em.createQuery("select c  from Cliente c", Cliente.class).getResultList();
         clientes.forEach(System.out::println);
         em.close();
