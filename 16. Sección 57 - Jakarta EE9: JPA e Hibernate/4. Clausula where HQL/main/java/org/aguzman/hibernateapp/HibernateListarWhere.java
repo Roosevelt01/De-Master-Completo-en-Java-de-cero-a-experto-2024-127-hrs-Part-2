e1399e0a412cf6e05280a3290f9d1048764e2bcd
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-11 12:28:24 -0400

    java2

--- 16. Sección 57 - Jakarta EE9: JPA e Hibernate/4. Clausula where HQL/main/java/org/aguzman/hibernateapp/HibernateListarWhere.java
+++ 16. Sección 57 - Jakarta EE9: JPA e Hibernate/4. Clausula where HQL/main/java/org/aguzman/hibernateapp/HibernateListarWhere.java
@@ -11,13 +11,25 @@ public class HibernateListarWhere {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
+        // 1. Obtener el EntityManager
         EntityManager em = JpaUtil.getEntityManager();
+        
+        // 2. Crear la consulta HQL con un parámetro posicional
         Query query = em.createQuery("select c from Cliente c where c.formaPago=?1", Cliente.class);
+
         System.out.println("Ingresa una forma de pago: ");
         String pago = s.next();
+        
+        // 3. Asignar el valor al parámetro de la consulta
         query.setParameter(1, pago);
+       
+        // 4. Ejecutar la consulta y obtener un único resultado
         Cliente c = (Cliente) query.getSingleResult();
+        
+        
         System.out.println(c);
+        
+        // 5. Cerrar el EntityManager
         em.close();
     }
 }
