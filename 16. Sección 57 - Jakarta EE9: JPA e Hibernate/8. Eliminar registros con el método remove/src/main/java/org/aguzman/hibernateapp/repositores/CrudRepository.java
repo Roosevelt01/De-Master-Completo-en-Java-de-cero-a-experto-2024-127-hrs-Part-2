fa995ee04fdce05b04f22cebc2c2ea734c12ca88
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-12 16:48:19 -0400

    java2

--- 16. Sección 57 - Jakarta EE9: JPA e Hibernate/8. Eliminar registros con el método remove/src/main/java/org/aguzman/hibernateapp/repositores/CrudRepository.java
+++ 16. Sección 57 - Jakarta EE9: JPA e Hibernate/8. Eliminar registros con el método remove/src/main/java/org/aguzman/hibernateapp/repositores/CrudRepository.java
@@ -2,9 +2,19 @@ package org.aguzman.hibernateapp.repositores;
 
 import java.util.List;
 
+// Interfaz genérica para operaciones CRUD. 'T' representa el tipo de entidad.
 public interface CrudRepository<T>{
+    // Devuelve una lista de todas las entidades del tipo 'T'.
     List<T> listar();
+
+    // Busca y devuelve una entidad por su ID.
+    // 'id' es el identificador único de la entidad.
     T porId(Long id);
+
+    // Guarda una entidad nueva o existente.
+    // Utiliza 'persist' para nuevos registros y 'merge' para actualizaciones.
     void guardar(T t);
+
+    // Elimina una entidad por su ID.
     void eliminar(Long id);
 }
