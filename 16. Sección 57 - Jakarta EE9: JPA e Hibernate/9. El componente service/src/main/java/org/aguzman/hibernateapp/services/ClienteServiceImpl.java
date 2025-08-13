38730b2f9dedda14472fd9a4c82e78ffd0a0952c
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-13 00:36:01 -0400

    java2

--- 16. Sección 57 - Jakarta EE9: JPA e Hibernate/9. El componente service/src/main/java/org/aguzman/hibernateapp/services/ClienteServiceImpl.java
+++ 16. Sección 57 - Jakarta EE9: JPA e Hibernate/9. El componente service/src/main/java/org/aguzman/hibernateapp/services/ClienteServiceImpl.java
@@ -12,26 +12,32 @@ public class ClienteServiceImpl implements ClienteService {
     private EntityManager em;
     private CrudRepository<Cliente> repository;
 
+    // El constructor recibe el EntityManager como dependencia.
+    // Con él, inicializamos una instancia de nuestro ClienteRepository.
     public ClienteServiceImpl(EntityManager em) {
         this.em = em;
         this.repository = new ClienteRepository(em);
     }
 
+    // El método listar solo delega la llamada al repositorio.
     @Override
     public List<Cliente> listar() {
         return repository.listar();
     }
 
+    // El método porId delega al repositorio y envuelve el resultado en un Optional.
     @Override
     public Optional<Cliente> porId(Long id) {
+        // Optional.ofNullable maneja el caso de que el resultado sea nulo.
         return Optional.ofNullable(repository.porId(id));
     }
 
+    // El método guardar maneja la transacción.
     @Override
     public void guardar(Cliente cliente) {
         try{
             em.getTransaction().begin();
-            repository.guardar(cliente);
+            repository.guardar(cliente);// La lógica de guardar/actualizar está en el repositorio.
             em.getTransaction().commit();
         }catch (Exception e){
             em.getTransaction().rollback();
@@ -39,11 +45,12 @@ public class ClienteServiceImpl implements ClienteService {
         }
     }
 
+    // El método eliminar también maneja la transacción.
     @Override
     public void eliminar(Long id) {
         try{
             em.getTransaction().begin();
-            repository.eliminar(id);
+            repository.eliminar(id); // La lógica de eliminación está en el repositorio.
             em.getTransaction().commit();
         }catch (Exception e){
             em.getTransaction().rollback();
