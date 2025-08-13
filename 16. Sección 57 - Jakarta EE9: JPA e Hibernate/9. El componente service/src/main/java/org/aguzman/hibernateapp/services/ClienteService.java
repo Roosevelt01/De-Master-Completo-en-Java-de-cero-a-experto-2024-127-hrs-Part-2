cfaef32e06835b38c91c3d361360accb4400e947
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-13 00:13:35 -0400

    java2

--- 16. Sección 57 - Jakarta EE9: JPA e Hibernate/9. El componente service/src/main/java/org/aguzman/hibernateapp/services/ClienteService.java
+++ 16. Sección 57 - Jakarta EE9: JPA e Hibernate/9. El componente service/src/main/java/org/aguzman/hibernateapp/services/ClienteService.java
@@ -6,8 +6,15 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ClienteService {
+    // Devuelve una lista de todos los clientes.
     List<Cliente> listar();
+
+    // Busca un cliente por su ID y lo devuelve en un objeto Optional.
     Optional<Cliente> porId(Long id);
+
+    // Guarda o actualiza un cliente.
     void guardar(Cliente cliente);
+
+    // Elimina un cliente por su ID.
     void eliminar(Long id);
 }
