6b8e1c26f24bfccc78d0800901f69fbb175665fe
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-14 18:30:24 -0400

    java2

---
+++ 17. Sección 58 - Jakarta EE9: JPA e Hibernate - Consultas JPQL/001. JPQL campos personalizados/src/main/java/org/aguzman/hibernateapp/HibernateQL.java
@@ -0,0 +1,53 @@
+package org.aguzman.hibernateapp;
+
+import org.aguzman.hibernateapp.entity.Cliente;
+import org.aguzman.hibernateapp.util.JpaUtil;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class HibernateQL {
+    public static void main(String[] args) {
+        EntityManager em = JpaUtil.getEntityManager();
+        System.out.println("============ Consultar todos ============");
+        List<Cliente> clientes = em.createQuery("select c from Cliente c", Cliente.class)
+                .getResultList();
+        clientes.forEach(System.out::println);
+
+        System.out.println("\n============ Consultar todos ============");
+        Cliente cliente = em.createQuery("select c from Cliente c where c.id=:id", Cliente.class)
+                .setParameter("id", 1L)
+                .getSingleResult();
+        System.out.println(cliente);
+
+        System.out.println("\n============ Consulta solo el nombre por el id ============");
+        Cliente nombreCliente = em.createQuery("select c from Cliente c where c.id=:id", Cliente.class)
+                .setParameter("id", 2L)
+                .getSingleResult();
+        System.out.println(nombreCliente);
+
+        System.out.println("\n============ Consultas por campo personalizado ============");
+        Object[] objetoCliente = em.createQuery("select c.id, c.nombre, c.apellido from Cliente c where c.id=:id", Object[].class)
+                .setParameter("id", 1L)
+                .getSingleResult();
+        Long id = (Long) objetoCliente[0];
+        String nombre = (String) objetoCliente[1];
+        String apellido = (String) objetoCliente[2];
+        System.out.println("id = "+ id +
+                "\nNombre: "+ nombre+
+                "\nApellido: " + apellido);
+
+        System.out.println("\n============ Consultas por campo personalizado lista ============");
+        List<Object[]> registros = em.createQuery("select c.id, c.nombre, c.apellido from Cliente c", Object[].class)
+                .getResultList();
+        registros.forEach(reg -> {
+            Long idCli = (Long) reg[0];
+            String nombreCli = (String) reg[1];
+            String apellidoCli = (String) reg[2];
+            System.out.println("id = " + idCli + "| nombre = "+ nombreCli + "| apellido = " + apellidoCli);
+        });
+
+        em.close();
+
+    }
+}
