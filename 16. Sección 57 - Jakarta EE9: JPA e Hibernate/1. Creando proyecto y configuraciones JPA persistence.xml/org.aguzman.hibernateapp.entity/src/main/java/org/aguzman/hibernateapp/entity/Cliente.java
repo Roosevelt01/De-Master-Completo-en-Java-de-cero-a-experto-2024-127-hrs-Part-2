66c75db822d1dd9fb2088710f00a2a5c6d2a3f24
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-14 13:40:36 -0400

    java2

--- 16. Sección 57 - Jakarta EE9: JPA e Hibernate/1. Creando proyecto y configuraciones JPA persistence.xml/org.aguzman.hibernateapp.entity/src/main/java/org/aguzman/hibernateapp/entity/Cliente.java
+++ 16. Sección 57 - Jakarta EE9: JPA e Hibernate/1. Creando proyecto y configuraciones JPA persistence.xml/org.aguzman.hibernateapp.entity/src/main/java/org/aguzman/hibernateapp/entity/Cliente.java
@@ -2,5 +2,5 @@ package org.aguzman.hibernateapp.entity;
 
 public class Cliente {
     // En la próxima lección, añadiremos atributos y anotaciones JPA
-    // como @Entity, @Id, @Column, etc.
+    // como @Entity, @Id, @Column, etc
 }
