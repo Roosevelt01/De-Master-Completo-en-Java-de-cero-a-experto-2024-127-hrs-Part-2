94fe570e2ec1367d6efcbe8ca0bba936ce24e31f
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-14 20:23:12 -0400

    java2

---
+++ 17. Sección 58 - Jakarta EE9: JPA e Hibernate - Consultas JPQL/002. JPQL instanciación dinámica de clase personalizada/src/main/java/org/aguzman/hibernateapp/entity/Cliente.java
@@ -0,0 +1,73 @@
+package org.aguzman.hibernateapp.entity;
+
+import javax.persistence.*;
+
+@Entity//Paso 1
+@Table(name="clientes")//Paso 2
+public class Cliente {
+    @Id//PAso 3
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//PAso 4
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+
+    @Column(name = "forma_pago")//PAso 5
+    private String formaPago;
+
+    public Cliente() {
+    }
+
+    //Paso 1
+    public Cliente(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Cliente(Long id, String nombre, String apellido, String formaPago) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.formaPago = formaPago;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    @Override
+    public String toString() {
+        return  "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", formaPago='" + formaPago;
+    }
+}
