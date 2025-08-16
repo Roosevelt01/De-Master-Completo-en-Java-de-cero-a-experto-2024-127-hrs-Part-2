c8b8f4ed12b65e5f16184e940da490fde5cbe7fd
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-16 15:40:09 -0400

    java2

--- 17. Sección 58 - Jakarta EE9: JPA e Hibernate - Consultas JPQL/005. Usando palabras claves between y order by de JPQL/src/main/java/org/aguzman/hibernateapp/HibernateQL.java
+++ 17. Sección 58 - Jakarta EE9: JPA e Hibernate - Consultas JPQL/005. Usando palabras claves between y order by de JPQL/src/main/java/org/aguzman/hibernateapp/HibernateQL.java
@@ -28,6 +28,27 @@ public class HibernateQL {
     }
 }
 
+//========================================================================================================
+
+1.1. BETWEEN con Tipos Numéricos
+
+// Imprime un encabezado para esta sección en la consola.
+System.out.println("\n============ Consulta por rangos numéricos ============");
+// Reasigna a la variable 'clientes' el resultado de una nueva consulta JPQL.
+clientes = em.createQuery("select c from Cliente c where c.id between 2 and 5", Cliente.class)
+        // Ejecuta la consulta y devuelve una lista con los resultados.
+        .getResultList();
+// Itera sobre la lista de clientes encontrados y los imprime en la consola.
+clientes.forEach(System.out::println);
+
+Resultado en Consola:
+
+Hibernate: select ... from clientes cliente0_ where cliente0_.id between 2 and 5
+id=2, nombre='John', apellido='Doe', formaPago='credito
+id=4, nombre='Pepa', apellido='Doe', formaPago='crédito
+id=5, nombre='Luna', apellido='Garcia', formaPago='debito
+
+
 
 
 
