22021431ccf826b7db5d9dbb92048d1aa2ca74a1
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-17 02:57:46 -0400

    java2

--- 17. Sección 58 - Jakarta EE9: JPA e Hibernate - Consultas JPQL/007. Subquery o subconsulta en JPQL/src/main/java/org/aguzman/hibernateapp/HibernateQL.java
+++ 17. Sección 58 - Jakarta EE9: JPA e Hibernate - Consultas JPQL/007. Subquery o subconsulta en JPQL/src/main/java/org/aguzman/hibernateapp/HibernateQL.java
@@ -10,170 +10,12 @@ import java.util.List;
 
 public class HibernateQL {
     public static void main(String[] args) {
-        EntityManager em = JpaUtil.getEntityManager();
-        System.out.println("============ Consultar todos ============");
-        List<Cliente> clientes = em.createQuery("select c from Cliente c", Cliente.class)
-                .getResultList();
-        clientes.forEach(System.out::println);
+        // ... (consultas anteriores) ...
 
-        System.out.println("\n============ Consultar todos ============");
-        Cliente cliente = em.createQuery("select c from Cliente c where c.id=:id", Cliente.class)
-                .setParameter("id", 1L)
-                .getSingleResult();
-        System.out.println(cliente);
-
-        System.out.println("\n============ Consulta solo el nombre por el id ============");
-        Cliente nombreCliente = em.createQuery("select c from Cliente c where c.id=:id", Cliente.class)
-                .setParameter("id", 2L)
-                .getSingleResult();
-        System.out.println(nombreCliente);
-
-        System.out.println("\n============ Consultas por campo personalizado ============");
-        Object[] objetoCliente = em.createQuery("select c.id, c.nombre, c.apellido from Cliente c where c.id=:id", Object[].class)
-                .setParameter("id", 1L)
-                .getSingleResult();
-        Long id = (Long) objetoCliente[0];
-        String nombre = (String) objetoCliente[1];
-        String apellido = (String) objetoCliente[2];
-        System.out.println("id = "+ id +
-                "\nNombre: "+ nombre+
-                "\nApellido: " + apellido);
-
-        System.out.println("\n============ Consultas por campo personalizado lista ============");
-        List<Object[]> registros = em.createQuery("select c.id, c.nombre, c.apellido from Cliente c", Object[].class)
-                .getResultList();
-        registros.forEach(reg -> {
-            Long idCli = (Long) reg[0];
-            String nombreCli = (String) reg[1];
-            String apellidoCli = (String) reg[2];
-            System.out.println("id = " + idCli + "| nombre = "+ nombreCli + "| apellido = " + apellidoCli);
-        });
-
-        System.out.println("\n============ Consulta por cliente y forma de pago ============");
-        registros = em.createQuery("select c, c.formaPago from Cliente c", Object[].class)
-                        .getResultList();
-        registros.forEach(reg -> {
-            Cliente c = (Cliente) reg[0];
-            String formaPago = (String) reg[1];
-            System.out.println("formaPago = " + formaPago + ", " + c);
-        });
-
-        System.out.println("\n============ Consulta que devuelve objeto entity de una clase personalizada ============");
-        clientes = em.createQuery("select new Cliente(c.nombre, c.apellido) from Cliente c", Cliente.class)
-                        .getResultList();
-        clientes.forEach(System.out::println);
-
-        System.out.println("\n============ Consulta que devuelve objeto Dto de una clase personalizada ============");
-        List<ClienteDto> clientesDtos = em.createQuery("select new org.aguzman.hibernateapp.dominio.ClienteDto(c.nombre, c.apellido) from Cliente c", ClienteDto.class)
-                .getResultList();
-        clientesDtos.forEach(System.out::println);
-
-        System.out.println("\n============ Consulta por cliente y forma de pago ============");
-        List<String> nombres = em.createQuery("select c.nombre from Cliente c", String.class)
-                        .getResultList();
-        nombres.forEach(System.out::println);
-
-        System.out.println("\n============ Consulta por cliente y forma de pago ============");
-        nombres = em.createQuery("select distinct(c.nombre) from Cliente c", String.class)
-                        .getResultList();
-        nombres.forEach(System.out::println);
-
-        System.out.println("\n============ Consulta con formas de pago única ============");
-        List<String> formasPago = em.createQuery("select distinct(c.formaPago) from Cliente c", String.class)
-                .getResultList();
-        formasPago.forEach(System.out::println);
-
-        System.out.println("\n============ Consulta con números de formas de pago única ============");
-        Long totalFormasPago = em.createQuery("select count(distinct(c.formaPago)) from Cliente c", Long.class)
-                .getSingleResult();
-        System.out.println(totalFormasPago);
-
-        System.out.println("\n============ Consulta con nombre y apellido concatenados ============");
-        //nombres = em.createQuery("select concat(c.nombre, ' ', c.apellido) as nombreCompleto from Cliente c", String.class)
-        //              .getResultList();
-
-        //Alternativa
-        nombres = em.createQuery("select c.nombre || ' ' || c.apellido as nombreCompleto from Cliente c", String.class)
-                .getResultList();
-
-        nombres.forEach(System.out::println);
-
-        System.out.println("\n============ Consulta con nombre y apellido concatenados en maýúsculas ============");
-        nombres = em.createQuery("select upper(concat(c.nombre, ' ', c.apellido)) as nombreCompleto from Cliente c", String.class)
-                      .getResultList();
-
-        nombres.forEach(System.out::println);
-
-        System.out.println("\n============ Consulta con nombre y apellido concatenados en minúsculas ============");
-        nombres = em.createQuery("select lower(concat(c.nombre, ' ', c.apellido)) as nombreCompleto from Cliente c", String.class)
-                .getResultList();
-
-        nombres.forEach(System.out::println);
-
-        System.out.println("\n============ Consulta para buscar por nombre ============");
-        String param= "and";
-        clientes = em.createQuery("select c from Cliente c where c.nombre like :parametro", Cliente.class)
-                        .setParameter("parametro", "%" + param + "%")
-                                .getResultList();
-        clientes.forEach(System.out::println);
-
-        System.out.println("\n============ Consulta por rangos ============");
-        //clientes = em.createQuery("select c from Cliente c where c.id between 2 and 5", Cliente.class).getResultList();
-        clientes = em.createQuery("select c from Cliente c where c.nombre between 'J' and 'Q'", Cliente.class).getResultList();
-        clientes.forEach(System.out::println);
-
-        System.out.println("\n============ Consulta con orden ============");
-        //clientes = em.createQuery("select c from Cliente c order by c.nombre asc", Cliente.class).getResultList();
-        //clientes = em.createQuery("select c from Cliente c order by c.nombre desc", Cliente.class).getResultList();
-        //clientes = em.createQuery("select c from Cliente c order by c.nombre desc, c. apellido desc", Cliente.class).getResultList();
-        clientes = em.createQuery("select c from Cliente c order by c.nombre asc, c. apellido desc", Cliente.class).getResultList();
-        clientes.forEach(System.out::println);
-
-        System.out.println("\n============ Consulta con total de registros de la tabla ============");
-        Long total = em.createQuery("select count(c) as total from Cliente c", Long.class).getSingleResult();
-        System.out.println(total);
-
-        System.out.println("\n============ Consulta con total de registros de la tabla ============");
-        Long minId = em.createQuery("select min(c.id) as minimo from Cliente c", Long.class).getSingleResult();
-        System.out.println(minId);
-
-        System.out.println("\n============ Consulta con max / último id ============");
-        Long maxId = em.createQuery("select max(c.id) as maximo from Cliente c", Long.class).getSingleResult();
-        System.out.println(maxId);
-
-        System.out.println("\n============ Consulta con nombre y su largo ============");
-        registros = em.createQuery("select c.nombre, length(c.nombre) from Cliente c", Object[].class).getResultList();
-        registros.forEach(reg -> {
-            String name = (String) reg[0];
-            Integer largo = (Integer) reg[1];
-            System.out.println("Nombre = " + name + " | largo = " +largo);
-        });
-
-        System.out.println("\n============ Consulta con el nombre más corto ============");
-        Integer minLargoNombre = em.createQuery("select min(length(c.nombre)) from Cliente c", Integer.class).getSingleResult();
-        System.out.println(minLargoNombre);
-
-        System.out.println("\n============ Consulta con el nombre más largo ============");
-        Integer maxLargoNombre = em.createQuery("select max(length(c.nombre)) from Cliente c", Integer.class).getSingleResult();
-        System.out.println(maxLargoNombre);
-
-        System.out.println("\n============ Consulta resumen funciones agregaciones count, min, max, avg y sum ============");
-        Object[] estadisticas = em.createQuery("select min(c.id), max(c.id), sum(c.id), count(c.id), avg(length(c.nombre)) from Cliente c", Object[].class)
-                        .getSingleResult();
-        Long min = (Long) estadisticas[0];
-        Long max = (Long) estadisticas[1];
-        Long sum = (Long) estadisticas[2];
-        Long count = (Long) estadisticas[3];
-        Double avg = (Double) estadisticas[4];
-
-        System.out.println("Min = " + min
-                + "\nmax = " + max
-                + "\nsum = " + sum
-                + "\ncount = " + count
-                + "\navg = " + avg);
-
-        //Paso 1
+        // Paso 1: Obtener el cliente con el nombre más corto y su longitud
         System.out.println("\n============ Consulta con el nombre más corto y su largo ============");
+        // La subconsulta `(select min(length(c.nombre)) ...)` encuentra la longitud mínima de todos los nombres.
+        // La consulta principal luego filtra y devuelve los clientes que coinciden con esa longitud.
         registros = em.createQuery("select c.nombre, length(c.nombre) from Cliente c where " +
                         "length(c.nombre) = (select min(length(c.nombre)) from Cliente c)", Object[].class)
                         .getResultList();
@@ -183,8 +25,9 @@ public class HibernateQL {
             System.out.println("Nombre = " + nom + "| Largo = " + largo);
         });
 
-        //Paso 2
+        // Paso 2: Obtener el cliente con el nombre más largo y su longitud
         System.out.println("\n============ Consulta con el nombre más largo y su largo ============");
+       // Se utiliza la misma lógica que en el paso anterior, pero con la función `max()` en la subconsulta.
         registros = em.createQuery("select c.nombre, length(c.nombre) from Cliente c where " +
                         "length(c.nombre) = (select max(length(c.nombre)) from Cliente c)", Object[].class)
                 .getResultList();
@@ -194,14 +37,18 @@ public class HibernateQL {
             System.out.println("Nombre = " + nom + "| Largo = " + largo);
         });
 
-        //Paso 3
+        // Paso 3: Obtener el último cliente registrado (el que tiene el ID más alto)
         System.out.println("\n============ Consulta para obtener el último registro ============");
+        // La subconsulta `(select max(c.id) ...)` encuentra el ID más alto.
+        // La consulta principal busca el cliente cuyo ID coincide con ese valor.
         Cliente ultimoCliente = em.createQuery("select c from Cliente c where c.id = (select max(c.id) from Cliente c)", Cliente.class)
                 .getSingleResult();
         System.out.println(ultimoCliente);
 
-        //Paso 4
+        // Paso 4: Consulta que usa la cláusula 'WHERE IN' con parámetros
         System.out.println("\n============ Consulta where in ============");
+        // La cláusula `in` permite filtrar por un conjunto de valores.
+        // Aquí se usa `:ids` como parámetro, el cual se enlaza con una lista de IDs.
         clientes = em.createQuery("select c from Cliente c where c.id in :ids", Cliente.class)
                         .setParameter("ids", Arrays.asList(1L, 2L, 9L, 6L))
                                 .getResultList();
