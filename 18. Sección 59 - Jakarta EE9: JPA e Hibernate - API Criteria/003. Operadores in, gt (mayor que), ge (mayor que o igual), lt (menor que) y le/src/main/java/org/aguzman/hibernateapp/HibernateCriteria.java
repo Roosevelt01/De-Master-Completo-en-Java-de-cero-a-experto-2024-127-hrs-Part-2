033f4bc39f2524fe14bdb5a9a4cc4ebe00bf4b37
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-19 18:34:24 -0400

    java2

--- 18. Sección 59 - Jakarta EE9: JPA e Hibernate - API Criteria/003. Operadores in, gt (mayor que), ge (mayor que o igual), lt (menor que) y le/src/main/java/org/aguzman/hibernateapp/HibernateCriteria.java
+++ 18. Sección 59 - Jakarta EE9: JPA e Hibernate - API Criteria/003. Operadores in, gt (mayor que), ge (mayor que o igual), lt (menor que) y le/src/main/java/org/aguzman/hibernateapp/HibernateCriteria.java
@@ -13,56 +13,7 @@ import java.util.List;
 
 public class HibernateCriteria {
     public static void main(String[] args) {
-        EntityManager em = JpaUtil.getEntityManager();
-
-        CriteriaBuilder criteria = em.getCriteriaBuilder();
-
-        CriteriaQuery<Cliente> query = criteria.createQuery(Cliente.class);
-
-        Root<Cliente> from = query.from(Cliente.class);
-
-        query.select(from);
-
-        List<Cliente> clientes = em.createQuery(query).getResultList();
-
-        clientes.forEach(System.out::println);
-
-        System.out.println("\n========== Listar Where equals ==========");
-//        query = criteria.createQuery(Cliente.class);//Paso 9
-//        from = query.from(Cliente.class);//Paso 10
-//        query.select(from).where(criteria.equal(from.get("nombre"), "Andres"));//Paso 11
-//        clientes = em.createQuery(query).getResultList();//Paso 12
-//        clientes.forEach(System.out::println);//Paso 13
-
-        //Alternativa
-        query = criteria.createQuery(Cliente.class);
-        from = query.from(Cliente.class);
-        ParameterExpression<String> nombreParam = criteria.parameter(String.class, "nombre");
-        query.select(from).where(criteria.equal(from.get("nombre"), nombreParam));
-
-        clientes = em.createQuery(query).setParameter("nombre","Andres").getResultList();
-        clientes.forEach(System.out::println);
-
-        System.out.println("\n========== Usando where like para buscar clientes por nombre ==========");
-        query = criteria.createQuery(Cliente.class);
-        from = query.from(Cliente.class);
-        query.select(from).where(criteria.like(from.get("nombre"), "%and%"));
-        clientes = em.createQuery(query).getResultList();
-        clientes.forEach(System.out::println);
-
-        query = criteria.createQuery(Cliente.class);
-        from = query.from(Cliente.class);
-        ParameterExpression<String> nombreParamLike = criteria.parameter(String.class, "nombreParam");
-        query.select(from).where(criteria.like(criteria.upper(from.get("nombre")), criteria.upper(nombreParamLike)));
-        clientes = em.createQuery(query).setParameter("nombreParam", "%jo%").getResultList();
-        clientes.forEach(System.out::println);
-
-        System.out.println("\n========== Usando usando where between para rangos ==========");
-        query = criteria.createQuery(Cliente.class);
-        from = query.from(Cliente.class);
-        query.select(from).where(criteria.between(from.get("id"), 2L, 6L));
-        clientes = em.createQuery(query).getResultList();
-        clientes.forEach(System.out::println);
+        //Resto del código
 
         //Ejemplo 1
         System.out.println("\n========== Consulta where in ==========");
