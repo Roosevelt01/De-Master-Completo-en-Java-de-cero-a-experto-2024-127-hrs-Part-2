583f797bebf941d3ed9b32634a9c6b4b207e72dc
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-19 18:33:36 -0400

    java2

--- 18. Sección 59 - Jakarta EE9: JPA e Hibernate - API Criteria/003. Operadores in, gt (mayor que), ge (mayor que o igual), lt (menor que) y le/src/main/java/org/aguzman/hibernateapp/HibernateCriteria.java
+++ 18. Sección 59 - Jakarta EE9: JPA e Hibernate - API Criteria/003. Operadores in, gt (mayor que), ge (mayor que o igual), lt (menor que) y le/src/main/java/org/aguzman/hibernateapp/HibernateCriteria.java
@@ -8,6 +8,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Root;
+import java.util.Arrays;
 import java.util.List;
 
 public class HibernateCriteria {
@@ -42,7 +43,6 @@ public class HibernateCriteria {
         clientes = em.createQuery(query).setParameter("nombre","Andres").getResultList();
         clientes.forEach(System.out::println);
 
-        //Ejemplo 1
         System.out.println("\n========== Usando where like para buscar clientes por nombre ==========");
         query = criteria.createQuery(Cliente.class);
         from = query.from(Cliente.class);
@@ -50,7 +50,6 @@ public class HibernateCriteria {
         clientes = em.createQuery(query).getResultList();
         clientes.forEach(System.out::println);
 
-        //Alternativa
         query = criteria.createQuery(Cliente.class);
         from = query.from(Cliente.class);
         ParameterExpression<String> nombreParamLike = criteria.parameter(String.class, "nombreParam");
@@ -58,11 +57,61 @@ public class HibernateCriteria {
         clientes = em.createQuery(query).setParameter("nombreParam", "%jo%").getResultList();
         clientes.forEach(System.out::println);
 
-        //Ejemplo 2
         System.out.println("\n========== Usando usando where between para rangos ==========");
         query = criteria.createQuery(Cliente.class);
         from = query.from(Cliente.class);
         query.select(from).where(criteria.between(from.get("id"), 2L, 6L));
+        clientes = em.createQuery(query).getResultList();
+        clientes.forEach(System.out::println);
+
+        //Ejemplo 1
+        System.out.println("\n========== Consulta where in ==========");
+        query = criteria.createQuery(Cliente.class);
+        from = query.from(Cliente.class);
+        //query.select(from).where(from.get("nombre").in("Andres", "John", "Lou"));
+        query.select(from).where(from.get("nombre").in(Arrays.asList("Andres", "John", "Lou")));//Alternativ
+        clientes = em.createQuery(query).getResultList();
+        clientes.forEach(System.out::println);
+
+        //Alternativa 2
+        query = criteria.createQuery(Cliente.class);
+        from = query.from(Cliente.class);
+        ParameterExpression<List> listParam = criteria.parameter(List.class, "nombres");
+        query.select(from).where(from.get("nombre").in(listParam));
+        clientes = em.createQuery(query)
+                .setParameter("nombres", Arrays.asList("Andres", "John", "Lou"))
+                .getResultList();
+        clientes.forEach(System.out::println);
+
+        //Ejemplo 2
+        System.out.println("\n========== Filtrar ge ==========");
+        query = criteria.createQuery(Cliente.class);
+        from = query.from(Cliente.class);
+        query.select(from).where(criteria.ge(from.get("id"), 3L ));
+        clientes = em.createQuery(query).getResultList();
+        clientes.forEach(System.out::println);
+
+        //Ejemplo 3
+        System.out.println("\n========== Filtrar gt ==========");
+        query = criteria.createQuery(Cliente.class);
+        from = query.from(Cliente.class);
+        query.select(from).where(criteria.gt(criteria.length(from.get("nombre")), 5L ));
+        clientes = em.createQuery(query).getResultList();
+        clientes.forEach(System.out::println);
+
+        //Ejemplo 2
+        System.out.println("\n========== Filtrar con le ==========");
+        query = criteria.createQuery(Cliente.class);
+        from = query.from(Cliente.class);
+        query.select(from).where(criteria.le(from.get("id"), 2L ));
+        clientes = em.createQuery(query).getResultList();
+        clientes.forEach(System.out::println);
+
+        //Ejemplo 2
+        System.out.println("\n========== Filtrar con lt ==========");
+        query = criteria.createQuery(Cliente.class);
+        from = query.from(Cliente.class);
+        query.select(from).where(criteria.lt(from.get("id"), 2L ));
         clientes = em.createQuery(query).getResultList();
         clientes.forEach(System.out::println);
 
