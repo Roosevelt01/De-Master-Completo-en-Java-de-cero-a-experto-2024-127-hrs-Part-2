ab1f6c12bfc09f68ba88524d36caeb6a0da72b79
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-19 17:51:12 -0400

    java2

---
+++ 18. Sección 59 - Jakarta EE9: JPA e Hibernate - API Criteria/src/main/java/org/aguzman/hibernateapp/repositores/CrudRepository.java
@@ -0,0 +1,10 @@
+package org.aguzman.hibernateapp.repositores;
+
+import java.util.List;
+
+public interface CrudRepository<T>{
+    List<T> listar();
+    T porId(Long id);
+    void guardar(T t);
+    void eliminar(Long id);
+}
