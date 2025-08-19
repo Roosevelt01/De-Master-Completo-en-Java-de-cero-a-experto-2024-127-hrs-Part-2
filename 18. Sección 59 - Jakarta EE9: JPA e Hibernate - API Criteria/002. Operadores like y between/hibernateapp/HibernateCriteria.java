0084998c113098d1ba58bd706e45f77f432f0955
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-19 13:27:28 -0400

    java2

--- 18. Sección 59 - Jakarta EE9: JPA e Hibernate - API Criteria/002. Operadores like y between/hibernateapp/HibernateCriteria.java
+++ 18. Sección 59 - Jakarta EE9: JPA e Hibernate - API Criteria/002. Operadores like y between/hibernateapp/HibernateCriteria.java
@@ -14,26 +14,38 @@ public class HibernateCriteria {
     public static void main(String[] args) {
         // Resto del código
 
-        //Ejemplo 1
+        // --- Ejemplo 1: Usando WHERE LIKE ---
         System.out.println("\n========== Usando where like para buscar clientes por nombre ==========");
+        // Paso 1: Se reinician los objetos 'query' y 'from' para una nueva consulta limpia.
         query = criteria.createQuery(Cliente.class);
         from = query.from(Cliente.class);
+        // Paso 2: // Se construye la cláusula LIKE con el método `criteria.like()`.
+        // Los caracteres '%' actúan como comodines para buscar cualquier coincidencia.
         query.select(from).where(criteria.like(from.get("nombre"), "%and%"));
-        clientes = em.createQuery(query).getResultList();
+
+        clientes = em.createQuery(query).getResultList();        
         clientes.forEach(System.out::println);
 
-        //Alternativa
+        //Alternativa: LIKE con UPPER y parámetros
         query = criteria.createQuery(Cliente.class);
         from = query.from(Cliente.class);
+        
+        // Paso 3: Se crea un `ParameterExpression` para usar un parámetro seguro.
         ParameterExpression<String> nombreParamLike = criteria.parameter(String.class, "nombreParam");
+        
+        // Paso 4: Se usa `criteria.upper()` para convertir el nombre de la entidad y el parámetro a mayúsculas
+        // antes de la comparación. Esto asegura que la búsqueda sea "case-insensitive".
         query.select(from).where(criteria.like(criteria.upper(from.get("nombre")), criteria.upper(nombreParamLike)));
+        
+        // Paso 5: El valor del parámetro incluye los comodines de búsqueda.
         clientes = em.createQuery(query).setParameter("nombreParam", "%jo%").getResultList();
         clientes.forEach(System.out::println);
 
-        //Ejemplo 2
+        // --- Ejemplo 2: Usando WHERE BETWEEN ---
         System.out.println("\n========== Usando usando where between para rangos ==========");
         query = criteria.createQuery(Cliente.class);
         from = query.from(Cliente.class);
+        // Paso 6: El método `criteria.between()` filtra los IDs que se encuentran en el rango [2, 6], ambos inclusive.
         query.select(from).where(criteria.between(from.get("id"), 2L, 6L));
         clientes = em.createQuery(query).getResultList();
         clientes.forEach(System.out::println);
