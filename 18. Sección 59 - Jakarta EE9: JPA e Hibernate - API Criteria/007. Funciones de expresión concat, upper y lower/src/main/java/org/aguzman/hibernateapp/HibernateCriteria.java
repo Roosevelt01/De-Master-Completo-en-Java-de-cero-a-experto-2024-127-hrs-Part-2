f7d97fb43e73df08a712365c56b3372928cc385f
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-21 20:31:08 -0400

    java2

--- 18. Sección 59 - Jakarta EE9: JPA e Hibernate - API Criteria/007. Funciones de expresión concat, upper y lower/src/main/java/org/aguzman/hibernateapp/HibernateCriteria.java
+++ 18. Sección 59 - Jakarta EE9: JPA e Hibernate - API Criteria/007. Funciones de expresión concat, upper y lower/src/main/java/org/aguzman/hibernateapp/HibernateCriteria.java
@@ -12,46 +12,46 @@ public class HibernateCriteria {
     public static void main(String[] args) {
         //Resto del código
 
-        // --- Ejemplo 1: Concatenar nombres y apellidos ---
-        System.out.println("\n========== Consulta por nombres y apellidos concatenados ==========");
-        // Paso 1: Se crea una nueva CriteriaQuery cuyo resultado será de tipo String.
-        queryString = criteria.createQuery(String.class);
-        
-        // Paso 2: Se establece el FROM en la entidad Cliente.
-        from = queryString.from(Cliente.class);
 
-        // Paso 3: Se define la selección de la consulta usando 'concat' de forma anidada.
-        queryString.select(
-                // La función 'concat' externa une el resultado de la interna con el apellido.
-                criteria.concat(criteria.concat(from.get("nombre"), " "),
-                // El segundo argumento de la 'concat' externa es el atributo 'apellido'.
-                from.get("apellido")));
+        //Ejemplo 1
+        System.out.println("\n========== Consulta de campos personalizados del entity cliente ==========");
+        CriteriaQuery<Object[]> queryObject = criteria.createQuery(Object[].class);
+        from = queryObject.from(Cliente.class);
+        queryObject.multiselect(from.get("id"), from.get("nombre"), from.get("apellido"));
+        List<Object[]> registros = em.createQuery(queryObject).getResultList();
+        registros.forEach(reg -> {
+            Long id = (Long) reg[0];
+            String nombre = (String ) reg[1];
+            String  apellido = (String ) reg[2];
+            System.out.println("id = "+ id +" | nombre: " +nombre+ " | apellido: "+apellido);
+        });
 
-        // Paso 4: Se ejecuta la consulta y se obtiene una lista de Strings con los nombres completos.
-        nombres = em.createQuery(queryString).getResultList();
-        
-        // Paso 5: Se imprimen los resultados en la consola.              
-        nombres.forEach(System.out::println);
+        //Ejemplo 2
+        System.out.println("\n========== Consulta de campos personalizados del entity cliente con where ==========");
+        queryObject = criteria.createQuery(Object[].class);
+        from = queryObject.from(Cliente.class);
+        queryObject.multiselect(from.get("id"), from.get("nombre"), from.get("apellido")).where(criteria.like(from.get("nombre"), "%lu%"));
+        registros = em.createQuery(queryObject).getResultList();
+        registros.forEach(reg -> {
+            Long id = (Long) reg[0];
+            String nombre = (String ) reg[1];
+            String  apellido = (String ) reg[2];
+            System.out.println("id = "+ id +" | nombre: " +nombre+ " | apellido: "+apellido);
+        });
 
-        // --- Ejemplo 2: Concatenar y convertir a mayúsculas ---
-        System.out.println("\n========== Consulta por nombres y apellidos concatenados en con upper ==========");
-        // Paso 1: Se crea una nueva CriteriaQuery cuyo resultado será de tipo String.
-        queryString = criteria.createQuery(String.class);
-        // Paso 2: Se establece el FROM en la entidad Cliente.
-        from = queryString.from(Cliente.class);
+        //Ejemplo 3
+        System.out.println("\n========== Consulta de campos personalizados del entity cliente con where id ==========");
+        queryObject = criteria.createQuery(Object[].class);
+        from = queryObject.from(Cliente.class);
+        queryObject.multiselect(from.get("id"), from.get("nombre"), from.get("apellido")).where(criteria.equal(from.get("id"), 2L));
+        Object[] registro = em.createQuery(queryObject).getSingleResult();
 
-        // Paso 3: Se define la selección, envolviendo toda la expresión de 'concat' anidada dentro de 'upper'.
-        queryString.select
-        
-                (criteria.upper // La función 'upper' externa convierte todo a mayúsculas.
-                        (criteria.concat(criteria.concat(from.get("nombre"), " "),
-                        from.get("apellido"))));
+        Long id = (Long) registro[0];
+        String nombre = (String ) registro[1];
+        String  apellido = (String ) registro[2];
+        System.out.println("id = "+ id +" | nombre: " +nombre+ " | apellido: "+apellido);
 
-        // Paso 4: Se ejecuta la consulta y se obtiene una lista de Strings con los nombres completos.
-        nombres = em.createQuery(queryString).getResultList();
-        
-        // Paso 5: Se imprimen los resultados en la consola.              
-        nombres.forEach(System.out::println);
+        em.close();
 
         em.close();
     }
