4846e8325da0f2f233d92f6f4ce52ca972a6a505
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-26 14:29:58 -0400

    java2

---
+++ 20. Sección 61 - Jakarta EE9: JPA e Hibernate - Asociaciones/002. Codificando ejemplo ManyToOne/src/main/java/org/aguzman/hibernateapp/entity/Auditoria.java
@@ -0,0 +1,44 @@
+package org.aguzman.hibernateapp.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+@Embeddable
+public class Auditoria {
+    @Column(name = "creado_en")
+    private LocalDateTime creadoEn;
+
+    @Column(name = "editado_en")
+    private LocalDateTime editadoEn;
+
+    @PrePersist
+    public void prePersist(){
+        System.out.println("Inicializar algo justo antesl del persist");
+        this.creadoEn = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        System.out.println("Inicializar algo justo antesl del update");
+        this.editadoEn = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreadoEn() {
+        return creadoEn;
+    }
+
+    public void setCreadoEn(LocalDateTime creadoEn) {
+        this.creadoEn = creadoEn;
+    }
+
+    public LocalDateTime getEditadoEn() {
+        return editadoEn;
+    }
+
+    public void setEditadoEn(LocalDateTime editadoEn) {
+        this.editadoEn = editadoEn;
+    }
+}
