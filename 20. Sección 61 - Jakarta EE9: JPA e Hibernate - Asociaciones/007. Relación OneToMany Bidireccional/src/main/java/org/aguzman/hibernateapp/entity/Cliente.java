0105dd0a935c16186dfaa3ed3ef9f8e84013fe32
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-29 17:26:18 -0400

    java2

---
+++ 20. Sección 61 - Jakarta EE9: JPA e Hibernate - Asociaciones/007. Relación OneToMany Bidireccional/src/main/java/org/aguzman/hibernateapp/entity/Cliente.java
@@ -0,0 +1,134 @@
+package org.aguzman.hibernateapp.entity;
+
+import org.hibernate.mapping.Join;
+
+import javax.persistence.*;
+import javax.xml.namespace.QName;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="clientes")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+
+    @Column(name = "forma_pago")
+    private String formaPago;
+
+    @Embedded
+    private Auditoria audit = new Auditoria();
+
+    @OneToMany(cascade =  CascadeType.ALL, orphanRemoval = true )
+    //@JoinColumn(name="id_cliente")
+    @JoinTable(name = "tbl_clientes_direcciones", joinColumns = @JoinColumn(name="id_cliente")
+    , inverseJoinColumns = @JoinColumn(name = "id_direccion")
+    , uniqueConstraints = @UniqueConstraint(columnNames = {"id_direccion"}))
+    private List<Direccion> direcciones;
+
+    //Paso 1
+    @OneToMany(cascade =  CascadeType.ALL, orphanRemoval = true, mappedBy = "cliente")
+    private List<Factura> facturas;
+
+    public Cliente() {
+        facturas = new ArrayList<>();//Paso 2
+        direcciones = new ArrayList<>();
+    }
+
+    public Cliente(String nombre, String apellido) {
+        this();
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Cliente(Long id, String nombre, String apellido, String formaPago) {
+        this();
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.formaPago = formaPago;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public List<Direccion> getDirecciones() {
+        return direcciones;
+    }
+
+    public void setDirecciones(List<Direccion> direcciones) {
+        this.direcciones = direcciones;
+    }
+
+    //Getter and setter
+    public Auditoria getAudit() {
+        return audit;
+    }
+
+    public void setAudit(Auditoria audit) {
+        this.audit = audit;
+    }
+
+    public List<Factura> getFacturas() {
+        return facturas;
+    }
+
+    public void setFacturas(List<Factura> facturas) {
+        this.facturas = facturas;
+    }
+
+    public Cliente addFactura(Factura factura){
+        this.facturas.add(factura);
+        factura.setCliente(this);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        LocalDateTime creado = this.audit != null? audit.getCreadoEn():null;
+        LocalDateTime editado = this.audit != null? audit.getCreadoEn():null;
+
+        return  " --> " + " id=" + id +
+                " | nombre='" + nombre + '\'' +
+                " | apellido='" + apellido + '\'' +
+                " | formaPago='" + formaPago + '\'' +
+                " | creadoEn='" + creado + '\'' +
+                " | editadoEn='" + editado + '\'' +
+                " | direcciones='" + direcciones + '\'' +
+                " | factura='" + facturas + '\'';//PAso 5
+    }
+}
