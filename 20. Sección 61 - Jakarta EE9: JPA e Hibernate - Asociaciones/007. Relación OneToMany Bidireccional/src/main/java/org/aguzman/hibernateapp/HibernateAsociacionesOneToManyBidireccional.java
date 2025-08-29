6a1385afb8eb15ba0dddd7c00c65a32230331753
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-29 17:28:38 -0400

    java2

---
+++ 20. Sección 61 - Jakarta EE9: JPA e Hibernate - Asociaciones/007. Relación OneToMany Bidireccional/src/main/java/org/aguzman/hibernateapp/HibernateAsociacionesOneToManyBidireccional.java
@@ -0,0 +1,42 @@
+package org.aguzman.hibernateapp;
+
+import org.aguzman.hibernateapp.entity.Cliente;
+import org.aguzman.hibernateapp.entity.Direccion;
+import org.aguzman.hibernateapp.entity.Factura;
+import org.aguzman.hibernateapp.util.JpaUtil;
+
+import javax.persistence.EntityManager;
+
+public class HibernateAsociacionesOneToManyBidireccional {
+    public static void main(String[] args) {
+        EntityManager em = JpaUtil.getEntityManager();
+
+        try{
+            em.getTransaction().begin();
+
+            Cliente cliente = new Cliente("Cata", "Edu");
+            cliente.setFormaPago("paypal");
+
+            Factura f1 = new Factura("compras de supermercado", 5000L);
+            Factura f2 = new Factura("compras de tecnología", 7000L);
+            /* cliente.getFacturas().add(f1);
+            cliente.getFacturas().add(f2);
+
+            f1.setCliente(cliente);
+            f2.setCliente(cliente);*/
+
+            //Alternativa
+            cliente.addFactura(f1)
+                   .addFactura(f2);
+
+            em.persist(cliente);
+            em.getTransaction().commit();
+            System.out.println(cliente);
+        }catch (Exception e){
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+    }
+}
