4607681c88aa05687a8e77cebe5aeeb6ab72038f
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-29 21:46:23 -0400

    java2

--- 20. Sección 61 - Jakarta EE9: JPA e Hibernate - Asociaciones/008. Eliminar objetos dependientes en OneToMany Bidireccional/src/main/java/org/aguzman/hibernateapp/HibernateAsociacionesOneToManyBidireccional.java
+++ 20. Sección 61 - Jakarta EE9: JPA e Hibernate - Asociaciones/008. Eliminar objetos dependientes en OneToMany Bidireccional/src/main/java/org/aguzman/hibernateapp/HibernateAsociacionesOneToManyBidireccional.java
@@ -9,22 +9,16 @@ import javax.persistence.EntityManager;
 
 public class HibernateAsociacionesOneToManyBidireccional {
     public static void main(String[] args) {
-        // Obtiene el EntityManager, nuestra interfaz para interactuar con el contexto de persistencia.
         EntityManager em = JpaUtil.getEntityManager();
 
         try{
-            // Inicia una nueva transacción. Todas las operaciones de escritura deben estar dentro de una.
             em.getTransaction().begin();
 
-            // Crea un nuevo Cliente.
             Cliente cliente = new Cliente("Cata", "Edu");
             cliente.setFormaPago("paypal");
 
-            // Crea dos nuevas Facturas.
             Factura f1 = new Factura("compras de supermercado", 5000L);
             Factura f2 = new Factura("compras de tecnología", 7000L);
-            
-            
             /* cliente.getFacturas().add(f1);
             cliente.getFacturas().add(f2);
 
@@ -32,26 +26,26 @@ public class HibernateAsociacionesOneToManyBidireccional {
             f2.setCliente(cliente);*/
 
             //Alternativa
-
-            // Usa el método ayudante para añadir las facturas.
-            // Esto sincroniza ambos lados de la relación de forma segura.
             cliente.addFactura(f1)
                    .addFactura(f2);
 
-            // Persiste únicamente el cliente. La cascada se encarga de las facturas.
             em.persist(cliente);
-
-            // Confirma la transacción, escribiendo los cambios (los 3 INSERTs) en la base de datos.
             em.getTransaction().commit();
-            // Imprime el estado final del objeto cliente y sus facturas asociadas.
+
+            //Nueva modificación
+            em.getTransaction().begin();
+
+            Factura f3 = new Factura("compras de supermercado", 5000L);
+            f3.setId(1L);
+            cliente.removeFactura(f3);
+            em.getTransaction().commit();
+            System.out.println(cliente);
             System.out.println(cliente);
         }catch (Exception e){
-            // Si ocurre un error, revierte la transacción.
             em.getTransaction().rollback();
             e.printStackTrace();
         }finally {
-            // Cierra el EntityManager para liberar la conexión y los recursos.
-            em.close(); 
+            em.close();
         }
     }
 }
