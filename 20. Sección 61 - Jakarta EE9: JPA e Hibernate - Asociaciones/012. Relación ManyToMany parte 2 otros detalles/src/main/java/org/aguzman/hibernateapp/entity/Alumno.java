ab62b401d5804927e10d114d63f8391f6553f6e4
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-01 20:09:55 -0400

    java2

--- 20. Sección 61 - Jakarta EE9: JPA e Hibernate - Asociaciones/012. Relación ManyToMany parte 2 otros detalles/src/main/java/org/aguzman/hibernateapp/entity/Alumno.java
+++ 20. Sección 61 - Jakarta EE9: JPA e Hibernate - Asociaciones/012. Relación ManyToMany parte 2 otros detalles/src/main/java/org/aguzman/hibernateapp/entity/Alumno.java
@@ -7,22 +7,23 @@ import java.util.List;
 @Entity
 @Table(name = "alumnos")
 public class Alumno {
-
     @Id
-    // Se declara la relación Many-to-Many
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nombre;
     private String apellido;
 
-    // Se declara la relación Many-to-Many
+    //Modicacion de la variable cursos
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "tbl_alumnos_cursos", joinColumns = @JoinColumn(name="alumno_id"),
+            inverseJoinColumns = @JoinColumn(name = "curso_id"),
+            uniqueConstraints = @UniqueConstraint(columnNames = {"alumno_id", "curso_id"}))
     private List<Curso> cursos;
 
-    //El resto del código
-
-    public Alumno() {this.cursos = new ArrayList<>(); }
+    public Alumno() {
+        this.cursos = new ArrayList<>();
+    }
 
     public Alumno(String nombre, String apellido) {
         this();
@@ -30,19 +31,37 @@ public class Alumno {
         this.apellido = apellido;
     }
 
-    //Getter and Setter
+    public Long getId() {
+        return id;
+    }
 
-    public Long getId() { return id; }
-    public void setId(Long id) {this.id = id; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getNombre() {return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getNombre() {
+        return nombre;
+    }
 
-    public String getApellido() { return apellido; }
-    public void setApellido(String apellido) { this.apellido = apellido; }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-    public List<Curso> getCursos() { return cursos; }
-    public void setCursos(List<Curso> cursos) {this.cursos = cursos; }
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
 
     @Override
     public String toString() {
