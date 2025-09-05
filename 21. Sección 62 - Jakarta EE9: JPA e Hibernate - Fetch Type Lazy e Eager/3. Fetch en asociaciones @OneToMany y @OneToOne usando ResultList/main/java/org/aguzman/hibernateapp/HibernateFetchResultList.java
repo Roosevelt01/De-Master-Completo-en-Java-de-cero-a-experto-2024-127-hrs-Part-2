73c3868476c4a8c85f493a1ddc433dc0bfbbf31e
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-04 23:36:39 -0400

    java2

--- 21. Sección 62 - Jakarta EE9: JPA e Hibernate - Fetch Type Lazy e Eager/3. Fetch en asociaciones @OneToMany y @OneToOne usando ResultList/main/java/org/aguzman/hibernateapp/HibernateFetchResultList.java
+++ 21. Sección 62 - Jakarta EE9: JPA e Hibernate - Fetch Type Lazy e Eager/3. Fetch en asociaciones @OneToMany y @OneToOne usando ResultList/main/java/org/aguzman/hibernateapp/HibernateFetchResultList.java
@@ -9,6 +9,9 @@ import java.util.List;
 public class HibernateFetchResultList {
     public static void main(String[] args) {
         EntityManager em = JpaUtil.getEntityManager();
+
+        // "DISTINCT" previene clientes duplicados en la lista final.
+        // "JOIN FETCH" carga las colecciones en la misma consulta, evitando el N+1.
         List<Cliente> clientes = em.createQuery("select distinct c from Cliente c left outer join fetch c.direcciones left outer join fetch c.detalle", Cliente.class).getResultList();
 
         clientes.forEach(c -> System.out.println(c.getNombre() + ", direcciones: "+ c.getDirecciones()));
