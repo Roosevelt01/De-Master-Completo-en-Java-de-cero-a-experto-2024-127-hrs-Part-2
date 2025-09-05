1677b2b32f2b4c157b64568f000d6e6eaf1f36a2
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-05 04:17:12 -0400

    java2

---
+++ 22. Sección 63 - Jakarta EE9: JPA e Hibernate - API criteria y Join Fetch/001. API Criteria y Join Fetch en asociación ManyToOne/main/java/org/aguzman/hibernateapp/HibernateFetchManyToMany.java
@@ -0,0 +1,24 @@
+package org.aguzman.hibernateapp;
+
+import org.aguzman.hibernateapp.entity.Alumno;
+import org.aguzman.hibernateapp.util.JpaUtil;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class HibernateFetchManyToMany {
+    public static void main(String[] args) {
+        // Paso 1: Obtener una instancia del EntityManager para interactuar con la base de datos.
+        EntityManager em = JpaUtil.getEntityManager();
+
+        // Paso 2: Crear una consulta JPQL personalizada con JOIN FETCH.
+        List<Alumno> alumnos = em.createQuery("select distinct a from Alumno a left outer join fetch a.cursos", Alumno.class)
+                .getResultList();
+        
+        // Paso 3: Iterar sobre la lista de alumnos e imprimir sus nombres y cursos.
+        alumnos.forEach(a -> System.out.println(a.getNombre() + " | cursos: " + a.getCursos()));
+        
+        // Paso 4: Cerrar el EntityManager para liberar recursos.
+        em.close();
+    }
+}
---
+++ 22. Sección 63 - Jakarta EE9: JPA e Hibernate - API criteria y Join Fetch/001. API Criteria y Join Fetch en asociación ManyToOne/main/java/org/aguzman/hibernateapp/entity/Factura.java
@@ -0,0 +1,80 @@
+package org.aguzman.hibernateapp.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="facturar")
+public class Factura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    private  String descripcion;
+    private  Long total;
+
+    //Paso 1
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="id_cliente")
+    private Cliente cliente;
+
+    public Factura() {
+    }
+
+    public Factura(String descripcion, Long total) {
+        this.descripcion = descripcion;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        return  "id=" + id +
+                " | descripción='" + descripcion + '\'' +
+                " | total=" + total;
+    }
+
+    //Nueva modificación
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        Factura factura = (Factura) object;
+        return Objects.equals(id, factura.id) && Objects.equals(descripcion, factura.descripcion) && Objects.equals(total, factura.total);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, descripcion, total);
+    }
+}
