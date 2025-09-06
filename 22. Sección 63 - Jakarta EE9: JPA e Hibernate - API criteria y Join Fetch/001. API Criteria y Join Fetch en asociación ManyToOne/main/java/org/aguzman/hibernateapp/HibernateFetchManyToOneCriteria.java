a8f341861b7ca13a29cc2740e20c906f22475458
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-06 16:11:36 -0400

    java2

--- 22. Sección 63 - Jakarta EE9: JPA e Hibernate - API criteria y Join Fetch/001. API Criteria y Join Fetch en asociación ManyToOne/main/java/org/aguzman/hibernateapp/HibernateFetchManyToOneCriteria.java
+++ 22. Sección 63 - Jakarta EE9: JPA e Hibernate - API criteria y Join Fetch/001. API Criteria y Join Fetch en asociación ManyToOne/main/java/org/aguzman/hibernateapp/HibernateFetchManyToOneCriteria.java
@@ -10,16 +10,27 @@ import java.util.List;
 
 public class HibernateFetchManyToOneCriteria {
     public static void main(String[] args) {
+        // Paso 1: Inicializar el EntityManager y el CriteriaBuilder.
         EntityManager em = JpaUtil.getEntityManager();
-
         CriteriaBuilder cb = em.getCriteriaBuilder();
+        
+        // Paso 2: Crear el objeto CriteriaQuery y definir la entidad raíz (FROM).
         CriteriaQuery<Factura> query = cb.createQuery(Factura.class);
         Root<Factura> factura = query.from(Factura.class);
-        Join<Factura, Cliente> cliente = (Join)factura.fetch("cliente", JoinType.LEFT);
+        
+        // Paso 3: Realizar un JOIN FETCH a las colecciones del cliente.
+        // Se utiliza 'fetch' para poblar las relaciones en el objeto de la entidad.
+        cliente.fetch("direcciones", JoinType.LEFT);
         cliente.fetch("detalle", JoinType.LEFT);
+
+        // Paso 4: Construir la consulta completa con SELECT y DISTINCT.
         query.select(factura).where(cb.equal(cliente.get("id"), 1L));
+
+        // Paso 5: Ejecutar la consulta y procesar los resultados.
         List<Factura> facturas = em.createQuery(query).getResultList();
         facturas.forEach(f-> System.out.println(f.getDescripcion() + " | cliente: "+ f.getCliente().getNombre()));
+
+        // Paso 6: Cerrar el EntityManager.
         em.close();
     }
 }
