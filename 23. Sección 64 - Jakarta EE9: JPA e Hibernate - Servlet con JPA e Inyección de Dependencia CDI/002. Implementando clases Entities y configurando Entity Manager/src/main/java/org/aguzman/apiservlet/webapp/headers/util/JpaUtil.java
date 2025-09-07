eb47b76c1bd6e12236aae27c3c5d229ee3c53221
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-07 11:24:37 -0400

    java2

--- 23. Sección 64 - Jakarta EE9: JPA e Hibernate - Servlet con JPA e Inyección de Dependencia CDI/002. Implementando clases Entities y configurando Entity Manager/src/main/java/org/aguzman/apiservlet/webapp/headers/util/JpaUtil.java
+++ 23. Sección 64 - Jakarta EE9: JPA e Hibernate - Servlet con JPA e Inyección de Dependencia CDI/002. Implementando clases Entities y configurando Entity Manager/src/main/java/org/aguzman/apiservlet/webapp/headers/util/JpaUtil.java
@@ -5,12 +5,15 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class JpaUtil {
+    // Paso 1: Atributo estático y final para el EntityManagerFactory.
     private static final EntityManagerFactory entiryManagerFactory = buildEntityManagerFactory();
 
+    // Paso 2: Método privado para construir la instancia.
     private static EntityManagerFactory buildEntityManagerFactory() {
         return Persistence.createEntityManagerFactory("ejemploJpa");
     }
 
+    // Paso 3: Método público para obtener un EntityManager por cada solicitud.
     public static EntityManager getEntityManager(){
         return  entiryManagerFactory.createEntityManager();
     }
